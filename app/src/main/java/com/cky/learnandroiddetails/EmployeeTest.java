e8177da3eb4135abf6aaf2f05626848e3bb9bc73
cuikangyuan <cuikangyuan@outlook.com>
2016-07-04 22:11:54 +0800

    update java core

---
+++ app/src/main/java/com/cky/learnandroiddetails/EmployeeTest.java
@@ -0,0 +1,23 @@
+package com.cky.learnandroiddetails;
+
+/**
+ * Created by cuikangyuan on 16/7/4.
+ */
+
+/*
+*文件名必须和 public 类的名字匹配mploy
+*
+* 在一个源文件中 只能有一个公有类 但可以有任意个非公有类
+*
+* 显式参数 明显的列在方法声明中
+* 隐式参数 没有出现在方法声明中 可能是方法名前的类对象(关键字this表示隐式参数)
+*
+*
+* */
+public class EmployeeTest {
+
+    class A {
+
+    }
+}
+
