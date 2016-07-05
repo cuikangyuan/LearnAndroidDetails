ebeb88912dbc65ef5551df7d78a478a46650f355
cuikangyuan <cuikangyuan@outlook.com>
2016-07-05 21:39:14 +0800

    增加 自绘控件 组合控件学习笔记

--- app/src/main/java/com/cky/learnandroiddetails/TestLayoutActivity.java
+++ app/src/main/java/com/cky/learnandroiddetails/TestLayoutActivity.java
@@ -2,10 +2,6 @@ package com.cky.learnandroiddetails;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewParent;
 import android.widget.LinearLayout;
 
 public class TestLayoutActivity extends AppCompatActivity {
@@ -41,6 +37,8 @@ public class TestLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        /*
         mainLayout = (LinearLayout) findViewById(R.id.main_layout);
         LayoutInflater layoutInflater = LayoutInflater.from(this);
         View buttonLayout = layoutInflater.inflate(R.layout.button_layout, null);
@@ -48,5 +46,6 @@ public class TestLayoutActivity extends AppCompatActivity {
 
         ViewParent viewParent = mainLayout.getParent();
         Log.d(TAG, "The parent of mainLayout is " + viewParent);
+        */
     }
 }
