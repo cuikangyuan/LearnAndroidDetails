4506caea23077614b926a631faf1286db8da8b51
cuikangyuan <cuikangyuan@outlook.com>
2016-07-17 22:58:55 +0800

    新增 Git 相关

---
+++ app/src/main/java/com/cky/learnandroiddetails/GitDetail.java
@@ -0,0 +1,34 @@
+package com.cky.learnandroiddetails;
+
+/**
+ * Created by cuikangyuan on 16/7/17.
+ */
+public class GitDetail {
+
+    /*
+    * 1.
+    * git config --global user.name "xxx"
+    *
+    * git config --global user.email "xxx"
+    *
+    * git config --list
+    *
+    * 工作目录 暂存区域 Git仓库
+    *
+    * 在工作目录中添加 修改文件
+    * 将需要进行版本控制的文件放入暂存区域
+    * 将暂存区域的文件提交到Git仓库
+    *
+    * git init
+    *
+    * git add
+    *
+    * git commit -m
+    *
+    * git reset HEAD //恢复暂存区域
+    *
+    * git checkout -- xxx 使用暂存区域内容覆盖本地的文件
+    *
+    * git log
+    * */
+}
