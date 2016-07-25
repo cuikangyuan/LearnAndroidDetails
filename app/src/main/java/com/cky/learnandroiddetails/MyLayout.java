2577341868739818a2384bb487fe4ecbe1e84344
cuikangyuan <cuikangyuan@outlook.com>
2016-07-25 22:11:54 +0800

    更新 View  相关

--- app/src/main/java/com/cky/learnandroiddetails/MyLayout.java
+++ app/src/main/java/com/cky/learnandroiddetails/MyLayout.java
@@ -3,12 +3,18 @@ package com.cky.learnandroiddetails;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.widget.LinearLayout;
 
 /**
  * Created by cuikangyuan on 16/7/19.
  */
 public class MyLayout extends LinearLayout {
+
+    /*
+    *Scroller 弹性滑动对象
+    * */
+
 
     public MyLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -18,5 +24,28 @@ public class MyLayout extends LinearLayout {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         //return super.onInterceptTouchEvent(ev);
         return false;
+    }
+
+    /*
+    *VelocityTracker 速度追踪
+    * */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        VelocityTracker velocityTracker = VelocityTracker.obtain();
+        velocityTracker.addMovement(event);
+
+        velocityTracker.computeCurrentVelocity(1000);// 计算速度 一段时间内手指所划过的像素数 1000ms
+        //速度 = (终点位置 - 起点位置) / 时间
+        int xVelocity = (int) velocityTracker.getXVelocity();
+        int yVelocity = (int) velocityTracker.getYVelocity();
+
+        //重置回收 内存
+        velocityTracker.clear();
+        velocityTracker.recycle();
+
+
+
+        return super.onTouchEvent(event);
     }
 }
