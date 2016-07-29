3ce12f922fbfce649786b1a341724aa1a2a3b555
cuikangyuan <cuikangyuan@outlook.com>
2016-07-29 22:07:29 +0800

    初始化 MVP  相关

---
+++ app/src/main/java/com/cky/learnandroiddetails/MVP/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cky.learnandroiddetails.MVP;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cky.learnandroiddetails.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/cky/learnandroiddetails/MVP/LoginModel.java
@@ -0,0 +1,7 @@
+package com.cky.learnandroiddetails.MVP;
+
+/**
+ * Created by cuikangyuan on 16/7/29.
+ */
+public class LoginModel {
+}
