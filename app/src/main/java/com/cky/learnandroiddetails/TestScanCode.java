9efbd29a27cc3f709f6274356db5cd5386b19682
cuikangyuan <cuikangyuan@outlook.com>
2016-08-22 22:21:03 +0800

    新增 zxing 相关

---
+++ app/src/main/java/com/cky/learnandroiddetails/TestScanCode.java
@@ -0,0 +1,13 @@
+package com.cky.learnandroiddetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestScanCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_scan_code);
+    }
+}
