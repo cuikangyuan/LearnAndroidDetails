60238c43b3e52860e489c6ee4c36c9fe3f8e7ea1
cuikangyuan <cuikangyuan@outlook.com>
2016-08-24 21:46:59 +0800

    更新 zxing 使用相关代码

--- app/src/main/java/com/cky/learnandroiddetails/TestScanCode.java
+++ app/src/main/java/com/cky/learnandroiddetails/TestScanCode.java
@@ -1,13 +1,54 @@
 package com.cky.learnandroiddetails;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.uuzuche.lib_zxing.activity.CaptureActivity;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
 
 public class TestScanCode extends AppCompatActivity {
+
+    Button mScanCodeBtn;
+    private static final int REQUEST_CODE = 991;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_scan_code);
+
+        mScanCodeBtn = (Button)findViewById(R.id.btnScanCode);
+
+        mScanCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TestScanCode.this, CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            //处理扫描结果（在界面上显示）
+            if (null != data) {
+                Bundle bundle = data.getExtras();
+                if (bundle == null) {
+                    return;
+                }
+                if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_SUCCESS) {
+                    String result = bundle.getString(CodeUtils.RESULT_STRING);
+                    Toast.makeText(this, "解析结果:" + result, Toast.LENGTH_LONG).show();
+                } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
+                    Toast.makeText(TestScanCode.this, "解析二维码失败", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
