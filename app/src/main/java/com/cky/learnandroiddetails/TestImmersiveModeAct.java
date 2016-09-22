3f2f3af164fabaccb89dc0f0db143584f84b2215
cuikangyuan <cuikangyuan@51tiangou.com>
2016-09-22 15:05:40 +0800

    新增 将ImageView延伸至状态栏【未完成】

---
+++ app/src/main/java/com/cky/learnandroiddetails/TestImmersiveModeAct.java
@@ -0,0 +1,39 @@
+package com.cky.learnandroiddetails;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * 作者：cky
+ * 时间：2016/9/22 13:32
+ * 描述：测试沉浸式模式
+ */
+
+public class TestImmersiveModeAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_immersive_mode);
+        showImmersiveMode();
+
+    }
+    //测试无效!!
+    private void showImmersiveMode() {
+        //要求系统5.0+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = (View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+    }
+}
