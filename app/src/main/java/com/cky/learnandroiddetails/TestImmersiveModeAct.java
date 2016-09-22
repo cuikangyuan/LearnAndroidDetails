8ab290bf39b2c37ac158bd997a3b4b7cd168261a
cuikangyuan <cuikangyuan@51tiangou.com>
2016-09-22 15:49:45 +0800

    更新 ImageView延伸至状态栏

--- app/src/main/java/com/cky/learnandroiddetails/TestImmersiveModeAct.java
+++ app/src/main/java/com/cky/learnandroiddetails/TestImmersiveModeAct.java
@@ -7,22 +7,34 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.jaeger.library.StatusBarUtil;
 
 /**
  * 作者：cky
  * 时间：2016/9/22 13:32
  * 描述：测试沉浸式模式
+ * 参考：http://jaeger.itscoder.com/android/2016/03/27/statusbar-util.html
  */
 
 public class TestImmersiveModeAct extends AppCompatActivity {
+
+    ImageView mImageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mImageView = (ImageView) findViewById(R.id.iv_banner);
+
         setContentView(R.layout.activity_test_immersive_mode);
-        showImmersiveMode();
+        //showImmersiveMode();
+
+        StatusBarUtil.setTranslucentForImageView(TestImmersiveModeAct.this, 0, mImageView);
 
     }
-    //测试无效!!
+    //【废弃】
     private void showImmersiveMode() {
         //要求系统5.0+
         if (Build.VERSION.SDK_INT >= 21) {
