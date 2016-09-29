4f8492cd6fa596e544b72531997ffad5140a59a9
cuikangyuan <cuikangyuan@51tiangou.com>
2016-09-29 09:35:29 +0800

    新增 【Hot Fix 入门】

---
+++ app/src/main/java/com/cky/learnandroiddetails/HotFix/HotFixClassLoaderAct.java
@@ -0,0 +1,31 @@
+package com.cky.learnandroiddetails.HotFix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.cky.learnandroiddetails.R;
+
+public class HotFixClassLoaderAct extends AppCompatActivity {
+
+    private static final String TAG = HotFixClassLoaderAct.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_fix_class_loader);
+
+        LogOutClassLoaderName();
+    }
+
+    private void LogOutClassLoaderName() {
+        ClassLoader classLoader = HotFixClassLoaderAct.class.getClassLoader();
+
+        while (classLoader != null) {
+
+            Log.d(TAG, classLoader.toString());
+
+            classLoader = classLoader.getParent();
+        }
+    }
+}
