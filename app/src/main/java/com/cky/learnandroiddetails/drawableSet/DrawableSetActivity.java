5102b9f8a2896809393ae7d39af6851679cebe42
cuikangyuan <cuikangyuan@51tiangou.com>
2017-01-22 16:36:39 +0800

    shape学习

---
+++ app/src/main/java/com/cky/learnandroiddetails/drawableSet/DrawableSetActivity.java
@@ -0,0 +1,13 @@
+package com.cky.learnandroiddetails.drawableSet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawableSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_set);
+    }
+}
