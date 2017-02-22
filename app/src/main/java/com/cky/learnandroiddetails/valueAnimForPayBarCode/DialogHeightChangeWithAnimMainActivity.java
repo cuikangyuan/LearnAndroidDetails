0e894ae99498f46a031348e7eb5bb05483acf22a
cuikangyuan <cuikangyuan@51tiangou.com>
2017-02-22 10:34:10 +0800

    add value animation

--- app/src/main/java/com/cky/learnandroiddetails/valueAnimForPayBarCode/DialogHeightChangeWithAnimMainActivity.java
+++ app/src/main/java/com/cky/learnandroiddetails/valueAnimForPayBarCode/DialogHeightChangeWithAnimMainActivity.java
@@ -1,33 +1,72 @@
 package com.cky.learnandroiddetails.valueAnimForPayBarCode;
 
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.cky.learnandroiddetails.R;
 
 public class DialogHeightChangeWithAnimMainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_height_change_with_anim_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 initDialog();
+                //showAnim();
             }
         });
 
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAnim();
+            }
+        });
 
         //initDialog();
+    }
+
+    private void showAnim() {
+        final View rootLayout = findViewById(R.id.root_layout);
+
+        final ViewGroup.LayoutParams layoutParams = rootLayout.getLayoutParams();
+
+        int targetHeight = getHeight(rootLayout);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, targetHeight);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animatedValue = (int) animation.getAnimatedValue();
+
+                layoutParams.height = animatedValue;
+                Log.d("TAG", animatedValue + "");
+                rootLayout.setLayoutParams(layoutParams);
+            }
+        });
+
+        animator.setDuration(1000);
+
+        animator.start();
     }
 
     private void initDialog() {
@@ -41,7 +80,11 @@ public class DialogHeightChangeWithAnimMainActivity extends AppCompatActivity {
 
         dialog.setCanceledOnTouchOutside(true);
 
-        Window window = dialog.getWindow();
+        final Window window = dialog.getWindow();
+
+        //View dialogView = LayoutInflater.from(this).inflate(R.layout.height_change_dialog, null);
+
+        //dialogView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 
         window.setContentView(R.layout.height_change_dialog);
 
@@ -52,11 +95,55 @@ public class DialogHeightChangeWithAnimMainActivity extends AppCompatActivity {
         //AnimatorUtil.animHeightToView(this, dialigText, true, 1000);
 
         //animateOpen(dialogImage, 600);
-        ImageView dialogImage1 = (ImageView) window.findViewById(R.id.dialog_image1);
+        //ImageView dialogImage1 = (ImageView) window.findViewById(R.id.dialog_image1);
 
-        ImageView dialogImage2 = (ImageView) window.findViewById(R.id.dialog_image2);
+        //final ImageView dialogImage2 = (ImageView) window.findViewById(R.id.dialog_image2);
 
-        AnimatorUtil.animHeightToView(this, dialogImage2, true, 800);
+        //AnimatorUtil.animHeightToView(this, dialogImage2, true, 800);
+
+
+
+        final View rootLayout = window.findViewById(R.id.root_layout);
+
+        final ViewGroup.LayoutParams layoutParams = rootLayout.getLayoutParams();
+
+        int targetHeight = getHeight(rootLayout);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, targetHeight);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animatedValue = (int) animation.getAnimatedValue();
+
+                layoutParams.height = animatedValue;
+                Log.d("TAG", animatedValue + "");
+                rootLayout.setLayoutParams(layoutParams);
+
+                rootLayout.requestLayout();
+            }
+        });
+
+
+        animator.setDuration(1000);
+
+        animator.start();
+    }
+
+
+
+
+    private int getHeight(View view) {
+
+        int height = 0;
+
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.AT_MOST);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(1000, View.MeasureSpec.AT_MOST);
+
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+
+        return view.getMeasuredHeight();
+
     }
 
     private void animateOpen(View v, int mHiddenViewMeasuredHeight) {
