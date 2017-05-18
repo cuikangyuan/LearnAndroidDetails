9324ee6558fa5db186fac6c657837fab1180f38f
cuikangyuan <cuikangyuan@51tiangou.com>
2017-05-18 14:00:27 +0800

    learn delete item in list when usr iterator

--- app/src/main/java/com/cky/learnandroiddetails/CopyOrWriteListLearn/CopyOrWriteListMainActivity.java
+++ app/src/main/java/com/cky/learnandroiddetails/CopyOrWriteListLearn/CopyOrWriteListMainActivity.java
@@ -6,13 +6,16 @@ import android.util.Log;
 
 import com.cky.learnandroiddetails.R;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CopyOrWriteListMainActivity extends AppCompatActivity {
 
-    List<ListItem> mListItems = new CopyOnWriteArrayList<>();
+    //List<ListItem> mListItems = new CopyOnWriteArrayList<>();
 
+    List<ListItem> mListItems = new ArrayList<>();
     private static final String TAG = "list";
 
     @Override
@@ -33,6 +36,41 @@ public class CopyOrWriteListMainActivity extends AppCompatActivity {
 
             mListItems.add(item);
         }
+        /*
+        Log.d(TAG, "before");
+        for (ListItem listItem : mListItems) {
+            Log.d(TAG, listItem.id + "->" + listItem.name);
+        }
+        for (ListItem listItem : mListItems) {
+            listItem.name = "修改后的name";
+        }
+        Log.d(TAG, "after");
+        for (ListItem listItem : mListItems) {
+            Log.d(TAG, listItem.id + "->" + listItem.name);
+        }
+        */
+        /*
+        for (ListItem listItem : mListItems) {
+            if (listItem.id == 1) {
+                //遍历时删除 只有在使用 copyOnWrite 时才可以
+                mListItems.remove(listItem);
+            }
+        }
+        Log.d(TAG, "after");
+        for (ListItem listItem : mListItems) {
+            Log.d(TAG, listItem.id + "->" + listItem.name);
+        }
+        */
+        //迭代器
+        Iterator<ListItem> iterator = mListItems.iterator();
+        while (iterator.hasNext()) {
+
+            if (iterator.next().id == 1) {
+                iterator.remove();
+            }
+
+        }
+
         for (ListItem listItem : mListItems) {
             Log.d(TAG, listItem.id + "->" + listItem.name);
         }
