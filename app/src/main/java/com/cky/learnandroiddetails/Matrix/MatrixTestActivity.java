ad1ef9877f4ba4ba9fc986e1bd50d7ccdcbfe256
cuikangyuan <cuikangyuan@51tiangou.com>
2017-05-31 13:58:55 +0800

    learn matrix

--- app/src/main/java/com/cky/learnandroiddetails/Matrix/MatrixTestActivity.java
+++ app/src/main/java/com/cky/learnandroiddetails/Matrix/MatrixTestActivity.java
@@ -2,7 +2,9 @@ package com.cky.learnandroiddetails.Matrix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.cky.learnandroiddetails.ManifestPlaceHolder.Helper;
 import com.cky.learnandroiddetails.R;
 
 public class MatrixTestActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class MatrixTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matrix_test);
+
+
+        Log.d("buildConfig", Helper.getManifestPlaceHolder(Helper.A_HOST));
     }
 }
