8ff619aa7e7d9c7e9eb441d503f2012268aaded8
cuikangyuan <cuikangyuan@51tiangou.com>
2017-06-26 14:43:05 +0800

    add  fade in text view  example

--- app/src/main/java/com/cky/learnandroiddetails/PasswordEditTextView/PasswordEditTextMainActivity.java
+++ app/src/main/java/com/cky/learnandroiddetails/PasswordEditTextView/PasswordEditTextMainActivity.java
@@ -3,23 +3,40 @@ package com.cky.learnandroiddetails.PasswordEditTextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.cky.learnandroiddetails.R;
+import com.cky.learnandroiddetails.widget.FadeInTextView;
 import com.cky.learnandroiddetails.widget.SubmitButtonWithAnimationView;
 
 public class PasswordEditTextMainActivity extends AppCompatActivity {
 
+    FadeInTextView mFadeInTextView;
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_edit_text_main);
 
-
+/*
         final SubmitButtonWithAnimationView viewById = (SubmitButtonWithAnimationView) findViewById(R.id.submit_view);
         viewById.setListener(new SubmitButtonWithAnimationView.ButtonOnClickListener() {
             @Override
             public void onClick(View view) {
                 viewById.start();
+            }
+        });
+  */
+
+        mFadeInTextView = (FadeInTextView) findViewById(R.id.fade_text_view);
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFadeInTextView
+                        .setTextString("一二三四五七八九十一二三四五七八九十一二三四五七八九十一二三四五七八九十一二三四五七八九十一二三四五七八九十")
+                        .startAnimation();
             }
         });
     }
---
+++ app/src/main/java/com/cky/learnandroiddetails/widget/FadeInTextView.java
@@ -0,0 +1,152 @@
+package com.cky.learnandroiddetails.widget;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+
+/**
+ * Created by cuikangyuan on 2017/6/26.
+ */
+
+public class FadeInTextView extends TextView {
+
+    private StringBuffer mStringBuffer = new StringBuffer();
+    private RectF mTextRect = new RectF();
+
+    private ValueAnimator mValueAnimator;
+
+    private static final int DURATION = 300;
+
+    private String[] arr;
+
+    private int textCount;
+
+    private int currentIndex = -1;
+
+    private TextAnimationListener mTextAnimationListener;
+
+    public TextAnimationListener getTextAnimationListener() {
+        return mTextAnimationListener;
+    }
+
+    public void setTextAnimationListener(TextAnimationListener textAnimationListener) {
+        mTextAnimationListener = textAnimationListener;
+    }
+    public FadeInTextView(Context context) {
+        this(context, null);
+    }
+
+    public FadeInTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FadeInTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mStringBuffer != null) {
+            drawText(canvas, mStringBuffer.toString());
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    private void drawText(Canvas canvas, String content) {
+        mTextRect.left = getPaddingLeft();
+        mTextRect.top = getPaddingTop();
+        mTextRect.right = getWidth() - getPaddingRight();
+        mTextRect.bottom = getHeight() - getPaddingBottom();
+
+        Paint.FontMetricsInt fontMetricsInt = getPaint().getFontMetricsInt();
+        int baseLine = (int) (mTextRect.top + mTextRect.bottom - fontMetricsInt.top - fontMetricsInt.bottom) / 2;
+
+        canvas.drawText(content, getPaddingLeft(), baseLine, getPaint());
+    }
+
+
+    public FadeInTextView setTextString(String textString) {
+        if (!TextUtils.isEmpty(textString)) {
+            textCount = textString.length();
+
+            arr = new String[textCount];
+
+            for (int i = 0; i < textCount; i++) {
+                arr[i] = textString.substring(i, i+1);
+            }
+
+            initAnimation();
+        }
+        return this;
+    }
+
+    private void initAnimation() {
+        if (textCount == 0) {
+            return;
+        }
+
+        mValueAnimator = ValueAnimator.ofInt(0, textCount - 1);
+        mValueAnimator.setDuration(textCount * DURATION);
+        mValueAnimator.setInterpolator(new LinearInterpolator());
+        mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animatedValue = (int) animation.getAnimatedValue();
+                if (currentIndex != animatedValue) {
+
+                    mStringBuffer.append(arr[animatedValue]);
+                    currentIndex = animatedValue;
+
+                    if (currentIndex == textCount - 1) {
+                        if (mTextAnimationListener != null) {
+                            mTextAnimationListener.animationFinish();
+                        }
+                    }
+
+                    invalidate();
+                }
+
+            }
+        });
+    }
+
+    public FadeInTextView startAnimation() {
+        if (mValueAnimator != null) {
+            mStringBuffer.setLength(0);
+            currentIndex = -1;
+            mValueAnimator.start();
+        }
+        return this;
+    }
+
+    private FadeInTextView stopAnimation() {
+        if (mValueAnimator != null) {
+            mValueAnimator.end();
+        }
+        return this;
+    }
+
+    public interface TextAnimationListener {
+        void animationFinish();
+    }
+
+
+}
