6e04451ecd5406f588ec1dfb74d6316ee2cdd847
cuikangyuan <cuikangyuan@51tiangou.com>
2017-07-28 14:21:31 +0800

    add rengwuxian study demo

---
+++ app/src/main/java/com/cky/learnandroiddetails/rengwuxian/RengwuxianMainActivity.java
@@ -0,0 +1,15 @@
+package com.cky.learnandroiddetails.rengwuxian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cky.learnandroiddetails.R;
+
+public class RengwuxianMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rengwuxian_main);
+    }
+}
